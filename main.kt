1293c39726acf0c8f54c92808c371ca66b4fb550
Priyanshu4042 <priyanshusingh052004@gmail.com>
2024-02-22 01:20:49 +0530

    Bank account Project Part 1

--- main.kt
+++ main.kt
@@ -1,3 +1,24 @@
 fun main(){
+
+  println("Welcome to your banking system. ")
+  println("What type of account you would like to create ? ")
+  println("1. Debit account")
+  println("2. Credit account") 
+  println("3. Checking account")  
+  println("Choose an option 1, 2 or 3.")
+
+  var userChoice : Int = readln().toInt()
+   
+  println("The selected option is $userChoice, ")
+
+  var accountType = when (userChoice){
+    1 -> "Debit account"
+    2 -> "Credit account"
+    3 -> "Checking account"
+
+    else -> continue
+  }
+  
+  println("You have created a $accountType.")
   
 }
