cd02635a0c4f67afb220c3b39ecbccaf3507ece5
Priyanshu4042 <priyanshusingh052004@gmail.com>
2024-02-22 14:53:00 +0530

    project part 1 fixes

--- main.kt
+++ main.kt
@@ -1,14 +1,40 @@
+/* var accountBalance = 0
+
+
+fun withdraw(var amount : Int, var accountType : String){
+  if (accountType == "Debit account" && amount>accountBalance){
+    println("error balance not enough")
+    println("Account balance is $accountBalance")
+  }
+
+  accountBalance -= amount
+  println("$amount withdrawn from the account")
+  println("Account balance is $accountBalance")
+}
+
+fun deposit(var amount : Int){
+  accountBalance += amount
+  println("$amount deposited to the account")
+  println("Account balance is $accountBalance")
+}
+*/
+
+
+
 fun main(){
 
   println("Welcome to your banking system. ")
   println("What type of account you would like to create ? ")
   println("1. Debit account")
   println("2. Credit account") 
-  println("3. Checking account")  
-  println("Choose an option 1, 2 or 3.")
+  println("3. Checking account")
+  
+  
+  do {
+    println("Choose an option 1, 2 or 3.")
 
   var userChoice : Int = readln().toInt()
-   
+
   println("The selected option is $userChoice, ")
 
   var accountType = when (userChoice){
@@ -16,9 +42,19 @@ fun main(){
     2 -> "Credit account"
     3 -> "Checking account"
 
-    else -> continue
-  }
+    else -> "error invalid input"
+  } 
   
   println("You have created a $accountType.")
-  
+  }
+   while (userChoice !in 1..3 );
+
+
 }
+  
+  
+
+  
+
+
+
